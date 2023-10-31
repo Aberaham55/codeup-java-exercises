7df5b47470a1df2312acf835569b457aa2128702
Abraham Garcia <aberaham.garcia55@gmail.com>
2023-10-31 12:11:00 -0500

    completed java intro exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,17 +1,42 @@
+import java.sql.SQLOutput;
+
 public class HelloWorld {
 
     public static void main(String[] args){
-        String animal = "Dog";
-        String breed = "ShibaDoodle";
-        int weight = 70;
-        boolean cuteOrNot = false;
-        cuteOrNot = true;
-        String name = "MonsterTurtle";
-        System.out.println(name + "is a " + breed);
-        if (cuteOrNot) {
-            System.out.println(name + "is cute");
-        } else {
-            System.out.println(name + "is mean");
-        }
+        int x = 4;
+        x = x + 5;
+        System.out.println(x);
+//        int myFavoriteNumber = 20;
+//        System.out.println(myFavoriteNumber);
+        String myString = "My favorite number is";
+//        System.out.println(myString);
+        long myNumber = 123L;
+//        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+//        String animal = "Dog";
+//        String breed = "ShibaDoodle";
+//        int weight = 70;
+//        boolean cuteOrNot = false;
+//        cuteOrNot = true;
+//        String name = "MonsterTurtle";
+//        System.out.println(name + "is a " + breed);
+//        if (cuteOrNot) {
+//            System.out.println(name + "is cute");
+//        } else {
+//            System.out.println(name + "is mean");
+//        }
     }
+
+
 }
